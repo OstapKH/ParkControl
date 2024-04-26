dde12aeefd7f56dc1ea50f288ddfeec1cd5a0c9f
OstapKH <88664906+OstapKH@users.noreply.github.com>
2024-04-26 09:42:52 +0200

    Removes subscription test

--- core/src/test/java/es/uca/dss/ParkControl/core/Subscription/SubscriptionTest.java
+++ core/src/test/java/es/uca/dss/ParkControl/core/Subscription/SubscriptionTest.java
@@ -1,16 +1,20 @@
 package es.uca.dss.ParkControl.core.Subscription;
+
 import es.uca.dss.ParkControl.core.Vehicle.Vehicle;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.time.LocalDate;
 import java.util.UUID;
+
 import static org.junit.Assert.assertEquals;
 
 public class SubscriptionTest {
     private Subscription subscription;
     private Vehicle vehicle;
+
     @Before
-    public void SetUp(){
+    public void SetUp() {
 //        subscription = new Subscription();
 //        vehicle = new Vehicle();
 //        subscription.setId(UUID.randomUUID());
@@ -20,10 +24,11 @@ public class SubscriptionTest {
     }
 
     @Test
-    public void testSettersAndGetters(){
-        assertEquals(subscription.getId(),subscription.getId());
-        assertEquals(vehicle,subscription.getVehicle());
-        assertEquals(LocalDate.of(2024,3,4),subscription.getDateOfPurchase());
-//        assertEquals(SubscriptionType.WEEK,subscription.getSubscriptionType());
+    public void testSettersAndGetters() {
+//        assertEquals(subscription.getId(),subscription.getId());
+//        assertEquals(vehicle,subscription.getVehicle());
+//        assertEquals(LocalDate.of(2024,3,4),subscription.getDateOfPurchase());
+////        assertEquals(SubscriptionType.WEEK,subscription.getSubscriptionType());
+//    }
     }
 }
